dbaba5154509715cb8b90c3571b22df69dd1e24f
Amul <amul67142@gmail.com>
2026-05-18 14:56:32 +0530

    fix: update getURL to read server-side Vercel URL environment variable

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,6 +10,7 @@ export const getURL = () => {
     process?.env?.NEXT_PUBLIC_APP_URL ?? // Set this to your site URL in production env.
     process?.env?.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ?? // Automatically set by Vercel.
     process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ?? // Automatically set by Vercel (Server-side).
     'http://localhost:3000';
   // Make sure to include `https://` when not localhost.
   url = url.includes('http') ? url : `https://${url}`;
